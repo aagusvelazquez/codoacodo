8499a2b55c4dba9f9a5237af5c7ba712bbaeb5c3
Agus Velazquez <134516462+aagusvelazquez@users.noreply.github.com>
2023-05-31 15:24:34 -0300

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,30 @@
+package codoacodo;
+/**
+ * Comentarios de documentación
+ * Título del programa
+ * @autor Agustina Velazquez
+ * @version 1.0
+ */
+
+//Clase del 18/04/2023
+
+/*
+ * Temas:
+ * definicion de variables
+ * asignacion de valores
+ */
+public class Main {
+    public static void main(String[] args) {
+        int num1;
+        double num2;
+        String nombre;
+
+        num1 = 10;
+        num2 = 5.15;
+        nombre = "Juan";
+
+        System.out.println("El primer número es " + num1);
+        System.out.println("El segundo número es " + num2);
+        System.out.println("El nombre es " + nombre);
+    }
+}
