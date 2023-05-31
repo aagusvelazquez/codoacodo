0a044835fbb9f9cb031af3e035a66737324b1f1e
Agus Velazquez <134516462+aagusvelazquez@users.noreply.github.com>
2023-05-31 14:56:25 -0300

    Add files via upload

---
+++ primerHolaMundo.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //acá va el código
+
+        //Escribir "Hola Mundo"; en PSeInt
+        System.out.println("Hola Mundo!");
+        System.out.println("Bienvenidos a CAC Inicial");
+    }
+}
