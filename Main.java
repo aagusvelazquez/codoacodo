f8d638a85938147bdaff8a56523bca0f40583650
Agus Velazquez <134516462+aagusvelazquez@users.noreply.github.com>
2023-05-31 14:43:42 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package primerospasos;
+public class Main {
+    public static void main(String[] args) {
+        int num1;
+        String nombre;
+        double num2;
+
+        num1 = 10;
+        num2 = 12.3;
+        nombre = "Juan";
+
+        //Escribir "El numero es: ", num1; en PSeInt
+        System.out.println("El primer número es: " + num1);
+        System.out.println("El segundo número es: " + num2);
+        System.out.println("La suma de los dos números es: " + (num1 + num2));
+        System.out.println("El nombre es: " + nombre);
+    }
+}
